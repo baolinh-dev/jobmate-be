cf7eaf9238a62175736dbe90d96739cede75c3ba
baolinh <baolinhdev@gmail.com>
2025-11-28 21:32:34 +0700

    update

--- jobmate-be/app.js
+++ jobmate-be/app.js
@@ -1,6 +1,6 @@
-require('dotenv').config();  
-const PORT = process.env.PORT;
-
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+const cookieParser = require("cookie-parser");
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
@@ -11,14 +11,21 @@ const app = express();
 connectDB();
 
 // Middleware
+app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+
+// CORS configuration để gửi cookie từ frontend
+app.use(cors({
+  origin: "http://localhost:3000", // frontend domain
+  credentials: true // cho phép gửi cookie HttpOnly
+}));
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
-app.use('/api/jobs', require('./routes/jobRoutes')); // thêm route jobs
-app.use('/api/categories', require('./routes/categoryRoutes')); 
-app.use('/api/applications', require('./routes/applicationRoutes')); 
+app.use('/api/jobs', require('./routes/jobRoutes'));
+app.use('/api/categories', require('./routes/categoryRoutes'));
+app.use('/api/applications', require('./routes/applicationRoutes'));
 app.use('/api/dashboard', require('./routes/dashboardRoutes'));
+
 // Start server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
